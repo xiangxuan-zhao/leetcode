25795010f83f45220f10efc83ed52b8760b4881e
doremi_xiangxuan <https://git.code.tencent.com/ceteng-duolaimi/ExamServer.git>
2020-04-15 21:44:16 +0800

    移动零

--- src/main/java/com/zxx/one/MoveZero.java
+++ src/main/java/com/zxx/one/MoveZero.java
@@ -4,11 +4,22 @@ public class MoveZero {
 
     /**
      * [283]移动零
-     * 解法一
+     * 解法一: 双指针法
+     * 定义一个指针i，i用来遍历
+     * 指针j指向值为0的第一个元素
      * 思路
      * @param nums
      */
     public void moveZeroes(int[] nums) {
-
+        int j = 0;
+        for(int i = 0;i < nums.length; i++){
+            if(nums[i] != 0) {
+                if(i != j){
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
     }
 }
