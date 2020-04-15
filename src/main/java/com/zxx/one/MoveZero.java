dba8167613fcb62b11964437bfdaf12b405a0af4
xiangxuan <zhaoxiangxuan@123drm.com>
2020-04-15 11:52:15 +0800

    环境准备

---
+++ src/main/java/com/zxx/one/MoveZero.java
@@ -0,0 +1,14 @@
+package com.zxx.one;
+
+public class MoveZero {
+
+    /**
+     * [283]移动零
+     * 解法一
+     * 思路
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+
+    }
+}
---
+++ src/test/java/com/zxx/one/MoveZeroTest.java
@@ -0,0 +1,10 @@
+package com.zxx.one;
+
+import org.junit.Test;
+
+public class MoveZeroTest {
+
+    @Test
+    public void moveZeroes() {
+    }
+}
