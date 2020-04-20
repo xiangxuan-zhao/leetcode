f0c77a5b83812352b8cd336c6f8d0ab3c2c13569
xiangxuan <zhaoxiangxuan@123drm.com>
2020-04-20 08:33:39 +0800

    283移动零

--- src/main/java/com/zxx/one/MoveZero.java
+++ src/main/java/com/zxx/one/MoveZero.java
@@ -4,22 +4,36 @@ public class MoveZero {
 
     /**
      * [283]移动零
-     * 解法一: 双指针法
-     * 定义一个指针i，i用来遍历
-     * 指针j指向值为0的第一个元素
+     * 解法一
      * 思路
      * @param nums
      */
-    public void moveZeroes(int[] nums) {
-        int j = 0;
-        for(int i = 0;i < nums.length; i++){
-            if(nums[i] != 0) {
+    public void moveZeroes1(int[] nums) {
+        int i,j = 0;
+        for(i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
                 if(i != j){
                     nums[j] = nums[i];
                     nums[i] = 0;
                 }
                 j++;
             }
+        }
+    }
+
+    /**
+     * 解法二：
+     */
+    public void moveZeroes2(int[] nums){
+        int index = 0;
+        for(int num : nums){
+            if(num != 0){
+                nums[index++] = num;
+            }
+        }
+
+        while(index < nums.length){
+            nums[index] = 0;
         }
     }
 }
