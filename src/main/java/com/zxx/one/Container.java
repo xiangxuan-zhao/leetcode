c8f61e86865ab01fa514c35e13c58aa257df98ac
xiangxuan <zhaoxiangxuan@123drm.com>
2020-04-23 02:17:56 +0800

    完成两数之和、盛水最多的容器

--- src/main/java/com/zxx/one/Container.java
+++ src/main/java/com/zxx/one/Container.java
@@ -1,4 +1,18 @@
 package com.zxx.one;
 
 public class Container {
+    /**
+     * [11]盛最多水的容器
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int i = 0,j = height.length-1,res = 0;
+        while(i < j){
+            res = height[i] < height[j]?
+                    Math.max(res,(j - i) * height[i++]):
+                    Math.max(res,(j - i) * height[j--]);
+        }
+        return res;
+    }
 }
--- src/main/java/com/zxx/one/TwoSum.java
+++ src/main/java/com/zxx/one/TwoSum.java
@@ -1,4 +1,44 @@
 package com.zxx.one;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+    /**
+     * [1]两数之和
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int i,j = 0;
+        for(i = 0;i < nums.length -1; i++){
+            for(j = i + 1;j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 一遍哈希
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+
 }
