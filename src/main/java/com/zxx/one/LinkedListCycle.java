1f52cf307bd063dfc4137bc738ecc677e7939759
doremi_xiangxuan <https://git.code.tencent.com/ceteng-duolaimi/ExamServer.git>
2020-04-24 14:29:32 +0800

    环形链表

---
+++ src/main/java/com/zxx/one/LinkedListCycle.java
@@ -0,0 +1,8 @@
+package com.zxx.one;
+
+/**
+ * @author Administrator
+ * @date 2020/04/24
+ */
+public class LinkedListCycle {
+}
--- src/main/java/com/zxx/one/ThreeSum.java
+++ src/main/java/com/zxx/one/ThreeSum.java
@@ -1,4 +1,47 @@
 package com.zxx.one;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Administrator
+ */
 public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for(int k = 0; k < nums.length - 2; k++){
+            if(nums[k] > 0){
+                break;
+            }
+            if(k > 0 && nums[k] == nums[k-1]){
+                continue;
+            }
+            int i = k + 1;
+            int j = nums.length - 1;
+            while(i < j){
+                int sum = nums[k] + nums[i] + nums[j];
+                if(sum > 0){
+                    while(i < j && nums[i] == nums[++i]) {
+                        ;
+                    }
+                }else if(sum < 0){
+                    while(i < j && nums[j] == nums[--j]) {
+                        ;
+                    }
+                }else{
+                    res.add(new ArrayList<Integer>(Arrays.asList(nums[k],nums[i],nums[j])));
+                    while(i < j && nums[i] == nums[++i]) {
+                        ;
+                    }
+                    while(i < j && nums[j] == nums[--j]) {
+                        ;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 }
