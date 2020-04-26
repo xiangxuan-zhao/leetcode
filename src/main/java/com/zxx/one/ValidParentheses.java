5db4e75ea7d2ebf91688c0470bff9be741aba35b
xiangxuan <zhaoxiangxuan@123drm.com>
2020-04-27 01:21:07 +0800

    有效括号

---
+++ src/main/java/com/zxx/one/ValidParentheses.java
@@ -0,0 +1,27 @@
+package com.zxx.one;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if(s.isEmpty()){
+            return true;
+        }
+        Stack<Character> stack = new Stack<Character>();
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                stack.push(')');
+            }else if(c == '{'){
+                stack.push('}');
+            }else  if(c == '['){
+                stack.push(']');
+            }else if(stack.empty() || c != stack.pop()){
+                return false;
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
